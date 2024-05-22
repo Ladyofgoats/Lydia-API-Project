df1bcaa052b89a97b67062523d6f09db2e93ec0c
Ladyofgoats <will12emery@googlemail.com>
2024-05-22 14:09:05 +0000

    Started adding in the code.

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const API_KEY = "X3R7jd39klvnwMFyU-vyerTNvmI";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(getElementByID("results.Modal"));
+
+document.getElementById("status")addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    
+}
